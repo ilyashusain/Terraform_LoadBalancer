dd04400afd6b57fd7d78f3d8d0f5727cdeb2abae
ilyashusain <44235768+ilyashusain@users.noreply.github.com>
2019-01-23 12:17:33 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,47 @@
+# provider.tf variables
+        # provider block
 
+variable "project" {
+        default = "clarapcp-ea-ilyas-husein"
+}
+variable "region" {
+        default = "us-east1"
+}
+variable "credentials" {
+        default = "~/creds/serviceaccount.json"
+}
+
+# instance.tf variables
+        # resource block
+
+variable "name" {
+        default = "jenkins"
+}
+variable "machine_type" {
+        default = "g1-small"
+}
+variable "zone" {
+        default = "us-east1-c"
+}
+variable "zone2" {
+        default = "us-east1-b"
+}
+
+variable "image" {
+        default = "packer-1547982783"
+}
+variable "tags" {
+        default = ["ssh", "jenkins", "http-server"]
+}
+variable "network" {
+        default = "claranet-test"
+}
+variable "ssh_user" {
+        default = "terraform"
+}
+
+# firewall.tf variables
+	# resource block
+variable "allowed_ports" {
+        default = ["22"]
+}
